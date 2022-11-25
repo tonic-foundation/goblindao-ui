cf4c6c9ed39eb2e6dbe311c9f7d24f492c64d0cf
gevkhanyan <gevorg.azizkhanyan@gmail.com>
2022-11-25 18:12:09 +0400

    GOB-4 / small fix

--- components/VoteCard/VoteCardInfo/index.tsx
+++ components/VoteCard/VoteCardInfo/index.tsx
@@ -8,9 +8,13 @@ const VoteCardInfo: FC<{
 }> = ({ infoSubtitle, info, infoTitle, ...props }) => {
   return (
     <Card hover="pointer" tw="justify-between flex items-center" {...props}>
-      <p tw="font-semibold text-neutral-500">{infoTitle}</p>
+      <p tw="font-semibold text-neutral-500 dark:text-neutral-400">
+        {infoTitle}
+      </p>
       <div tw="flex flex-col justify-end text-right gap-1">
-        <p tw="text-sm text-neutral-400 font-light">{infoSubtitle}</p>
+        <p tw="text-sm text-neutral-400 dark:text-neutral-300 font-light">
+          {infoSubtitle}
+        </p>
         <p tw="font-semibold">{info}</p>
       </div>
     </Card>
