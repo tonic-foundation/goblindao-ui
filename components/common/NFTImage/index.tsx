c0eefffaa5f86165880def7900cc9270b7704897
gevkhanyan <gevorg.azizkhanyan@gmail.com>
2023-02-20 15:36:03 +0400

    NFT image

--- components/common/NFTImage/index.tsx
+++ components/common/NFTImage/index.tsx
@@ -1,8 +1,22 @@
 import { GOBLIN_IPFS_URL } from '@/config';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import Icon from '../Icon';
 
 const NFTImage: FC<{ tokenId: string }> = ({ tokenId, ...props }) => {
-  return <img {...props} src={`${GOBLIN_IPFS_URL}/${tokenId}.png`} alt="" />;
+  const [imgLoadError, setImgLoadError] = useState(false);
+
+  return !imgLoadError ? (
+    <img
+      onError={() => setImgLoadError(true)}
+      {...props}
+      src={`${GOBLIN_IPFS_URL}/${tokenId}.png`}
+      alt=""
+    />
+  ) : (
+    <div {...props} tw="flex items-center justify-center">
+      <Icon.Avatar tw="w-5 h-5" />
+    </div>
+  );
 };
 
 export default NFTImage;
